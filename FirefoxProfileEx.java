c599e1bf3861986f94310169b706ac4d26900afe
Kiran Puram <kiran1028@gmail.com>
2015-12-17 23:22:19 +0000

    updated two files

--- FirefoxProfileEx.java
+++ FirefoxProfileEx.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.firefox.internal.ProfilesIni;
-
+//checking the update
 public class FirefoxProfileEx {
 
 	public static void main(String[] args) {
--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 Selenium Test scripts for TestTriangleApp
 This video tutorial can be found at : https://www.youtube.com/watch?v=EYIvWWx1YDw&spfreload=1
 
-new data updated on 17th
+new data updated on 17th double check
